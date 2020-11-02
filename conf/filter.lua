372cfea068cffcde47ee88237e31b5b2421a5bbd
Indika Piyasena <indika.piyasena@gmail.com>
2020-11-02 16:15:40 +1000

    a very basic configuration

---
+++ conf/filter.lua
@@ -0,0 +1,16 @@
+local remote_user = ngx.var.remote_user
+local remote_addr = ngx.var.remote_addr
+local data = ngx.req.get_body_data()
+local uri = ngx.var.uri
+
+ngx.log(ngx.STDERR, remote_user)
+ngx.log(ngx.STDERR, remote_addr)
+ngx.log(ngx.STDERR, uri)
+ngx.log(ngx.STDERR, data)
+
+if string.match(uri, "beacon") then
+    ngx.say('This URL is blocked')
+    ngx.exit(403);
+else
+    ngx.say("OK!")
+end
